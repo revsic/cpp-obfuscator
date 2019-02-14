ee7d7823668dcec60bb6e8ea49c50a6a25661765
revsic <revsic99@gmail.com>
2019-02-15 02:20:32 +0900

    obfs, string: Add method string

--- obfuscator/impl/string_obfs.hpp
+++ obfuscator/impl/string_obfs.hpp
@@ -3,43 +3,52 @@
 
 #include <utility>
 
-template <size_t size, typename D>
-class StringObfs {
-public:
-    template <typename E, typename Ds, size_t... Idx>
-    constexpr StringObfs(char const* str,
+namespace obfs {
+    template <size_t size, typename D>
+    class String {
+    public:
+        template <typename E, typename Ds, size_t... Idx>
+        constexpr String(char const* str,
                          E&& encoder,
                          Ds&& decoder,
                          std::index_sequence<Idx...>):
-        str{ encoder(str[Idx])... },
-        decoder(std::forward<Ds>(decoder)) {
-        // Do Nothing
-    }
-
-    constexpr size_t len() const {
-        return size;
-    }
-
-    inline const char* decode() const {
-        for (size_t i = 0; i < size; ++i) {
-            str[i] = decoder(str[i]);
+            str{ encoder(str[Idx])... },
+            decoder(std::forward<Ds>(decoder)) {
+            // Do Nothing
         }
-        return str;
-    }
 
-private:
-    mutable char str[size];
-    D decoder;
-};
+        constexpr size_t len() const {
+            return size;
+        }
 
-template <size_t len, typename E, typename D>
-constexpr auto make_stringobfs(char const (&str)[len],
+        inline char const* decode() const {
+            for (size_t i = 0; i < size; ++i) {
+                str[i] = decoder(str[i]);
+            }
+            return str;
+        }
+
+    private:
+        mutable char str[size];
+        D decoder;
+    };
+
+    template <size_t len, typename E, typename D>
+    constexpr auto make_string(char const (&str)[len],
                                E&& encoder,
                                D&& decoder) {
-    return StringObfs<len, D>(str,
+        return String<len, D>(str,
                               std::forward<E>(encoder),
                               std::forward<D>(decoder),
                               std::make_index_sequence<len>());
+    }
+
+    template <size_t len, typename E, typename D>
+    inline char const* string(char const (&str)[len],
+                              E&& encoder,
+                              D&& decoder) {
+        return make_string(str, std::forward<E>(encoder), std::forward<D>(decoder)).decode();
+    }
 }
 
 #endif
