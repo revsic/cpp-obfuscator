1caba45765aab1f665a2114b4fd7ba970ff7d6ba
revsic <revsic99@gmail.com>
2019-04-06 04:42:38 +0900

    fsm: Fix sample

--- sample/state_machine.cpp
+++ sample/state_machine.cpp
@@ -11,15 +11,13 @@ EVENT(Trigger);
 STATE(state1); STATE(state2); STATE(state3); STATE(state4); STATE(state5);
 EVENT(event1); EVENT(event2); EVENT(event3); EVENT(event4); EVENT(event5);
 
-bool action() {
+void action() {
     std::cout << "Trigged" << std::endl;
-    return true;
 }
 
 int main() {
     using namespace obfs;
     using machine = StateMachine<
-        state1,
         Stage<state1, Next<event5 , state2>,
                       Next<event1 , state3>>,
         Stage<state2, Next<event2 , state4>>,
@@ -28,7 +26,10 @@ int main() {
                       Next<event3 , state5>>,
         Stage<state5, Next<Trigger, Final, action>>>;
 
-    machine::run<event5, event2, event3, Trigger>();
+    auto next1 = machine::run(state1{}, event5{});
+    auto next2 = machine::run(next1, event2{});
+    auto next3 = machine::run(next2, event3{});
+    auto next4 = machine::run(next3, Trigger{});
 
     return 0;
 }
